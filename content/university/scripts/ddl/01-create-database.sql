bd280bdd1f5afaf9827411b484c25b7f4e963aab
JonathanBecerra202 <jonathanandresb199@gmail.com>
2026-05-15 14:15:11 -0500

    chore(database): update database initialization script
    Update PostgreSQL initialization script with universitydb creation,
    UTF-8 locale configuration, admin user setup, and database privileges.

--- content/university/scripts/ddl/01-create-database.sql
+++ content/university/scripts/ddl/01-create-database.sql
@@ -1,1 +1,12 @@
-dsds
+CREATE USER univ_admin WITH PASSWORD 'xl.2026upbda!!';
+
+--02. Create Database (with ENCODING= 'UTF8',TEMPLATE=template0, OWNER = univ_admin)
+CREATE DATABASE universitydb WITH 
+    ENCODING='UTF8' 
+    LC_COLLATE='es_CO.UTF-8' 
+    LC_CTYPE='es_CO.UTF-8' 
+    TEMPLATE=template0 
+    OWNER = univ_admin;
+
+--03. Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE universitydb TO univ_admin;
